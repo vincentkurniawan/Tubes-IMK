5466e7cffe82cfdcc0263311a4b1b8afda4152ba
Kevin Sunjaya <kssunjaya@gmail.com>
2021-12-19 22:51:14 +0700

    Sort ISBN & Fix sort

--- script/home.js
+++ script/home.js
@@ -42,6 +42,127 @@ var books=[
   "price":"Rp. 89.000",
   "src":"../images/books_cover/7.png"
 }
+,{
+  "title":"Not Here to Be Liked",
+  "author":"Michelle Quach",
+  "isbn":"978006308363",
+  "rating":4.6,
+  "price":"Rp. 100.000",
+  "src":"../images/books_cover/6.png"
+}
+,{
+  "title":"Beyond The Black Door",
+  "author":"A. M. Strickland",
+  "isbn":"9781250198747",
+  "rating":4.45,
+  "price":"Rp. 65.000",
+  "src":"../images/books_cover/8.png"
+},{
+  "title":"Nightbooks",
+  "author":"J. A. White",
+  "isbn":"9780062560094",
+  "rating":4.3,
+  "price":"Rp. 97.000",
+  "src":"../images/books_cover/9.png"
+},{
+  "title":"Ginger Bread",
+  "author":"Helen Oyeyemi",
+  "isbn":"9780143197850",
+  "rating":4.25,
+  "price":"Rp. 92.000",
+  "src":"../images/books_cover/10.png"
+},{
+  "title":"Nobody Knows But You",
+  "author":"Anica Mrose Rissi",
+  "isbn":"9780062685315",
+  "rating":4.17,
+  "price":"Rp. 117.000",
+  "src":"../images/books_cover/11.png"
+},{
+  "title":"Lost in The Never Woods",
+  "author":"Aiden Thomas",
+  "isbn":"9781250313973",
+  "rating":4.02,
+  "price":"Rp. 75.000",
+  "src":"../images/books_cover/12.png"
+},{
+  "title":"Of White and Shady",
+  "author":"Robert Verlander",
+  "isbn":"9781925209297",
+  "rating":4.00,
+  "price":"Rp. 63.000",
+  "src":"../images/books_cover/13-new.png"
+},{
+  "title":"Before We Disappear",
+  "author":"Shaun David Hutchinson",
+  "isbn":"7485464485270",
+  "rating":3.9,
+  "price":"Rp. 89.000",
+  "src":"../images/books_cover/14.png"
+},{
+  "title":"The Inheritance Games",
+  "author":"Jennifer Lynn Barnes",
+  "isbn":"9780241476178",
+  "rating":3.8,
+  "price":"Rp. 76.750",
+  "src":"../images/books_cover/15.jpg"
+},{
+  "title":"The Bird",
+  "author":"Jules Michelet",
+  "isbn":"9780143197853",
+  "rating":3.65,
+  "price":"Rp. 92.000",
+  "src":"../images/books_cover/16.jpg"
+},{
+  "title":"Scavengers",
+  "author":"Darren Simpson",
+  "isbn":"9781474956024",
+  "rating":3.59,
+  "price":"Rp. 132.000",
+  "src":"../images/books_cover/17.jpg"
+},{
+  "title":"The Wild Girl",
+  "author":"Kate Forsyth",
+  "isbn":"9781483084152",
+  "rating":3.58,
+  "price":"Rp. 123.000",
+  "src":"../images/books_cover/18.jpg"
+},{
+  "title":"The Ninth Life",
+  "author":"RTaylor B. Barton",
+  "isbn":"9781335145888",
+  "rating":2.72,
+  "price":"Rp. 99.000",
+  "src":"../images/books_cover/19.png"
+},{
+  "title":"The Wood : The Life and Times of Cockshutt Wood",
+  "author":"John Lewis-Stempel",
+  "isbn":"9781528872195",
+  "rating":2.67,
+  "price":"Rp. 56.000",
+  "src":"../images/books_cover/20.png"
+},{
+  "title":"An Enchantment of Ravens",
+  "author":"Margaret Rogerson",
+  "isbn":"9781536447545",
+  "rating":2.55,
+  "price":"Rp. 106.000",
+  "src":"../images/books_cover/21.jpg"
+},{
+  "title":"The Loneliest Girl in The Universe",
+  "author":"Lauren James",
+  "isbn":"9780062660268",
+  "rating":2.45,
+  "price":"Rp. 72.000",
+  "src":"../images/books_cover/22.jpg"
+},{
+  "title":"Carbonel",
+  "author":"Barbara Sleigh",
+  "isbn":"9780722664759",
+  "rating":2.55,
+  "price":"Rp. 68.000",
+  "src":"../images/books_cover/23.jpg"
+}
 ];
 var descending = false;
 
@@ -88,8 +209,10 @@ function onSubmit(e){
   let searchTextbox = document.getElementById("t-search");
   console.log(searchTextbox);
   if(searchTextbox.value == "") window.location.replace("Home.html");
+
   if(searchFilter.value == "0") window.location.replace("HomeSearchLittleWoman.html");
-  else window.location.replace("HomeSearchRachelHartman.html");
+  else if (searchFilter.value == "1") window.location.replace("HomeSearchRachelHartman.html");
+  else if (searchFilter.value == "2") window.location.replace("HomeSearchISBN.html");
 }
 
 function onCardClick(e){
